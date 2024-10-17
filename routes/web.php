bebad2e127a4a8449ee26a2cd2d8c590124e45e8
farenhas <faren.haseena@gmail.com>
2024-10-17 07:27:53 +0700

    ui landing page

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    // Profile routes
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Item routes
     Route::resource('items', ItemsController::class);
 
-    // Supplier routes
+    
     Route::get('/suppliers', [SuppliersController::class, 'index'])->name('suppliers.index');
     Route::get('/suppliers/create', [SuppliersController::class, 'create'])->name('suppliers.create');
     Route::post('/suppliers', [SuppliersController::class, 'store'])->name('suppliers.store');
