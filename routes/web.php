6db860282d05f11a1e35381ee9cc8cd098c5fad5
farenhas <127321716+farenhas@users.noreply.github.com>
2024-10-17 13:42:12 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemsController; 
 use App\Http\Controllers\SuppliersController; 
 
-
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,20 +14,23 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Item routes
+    Route::resource('items', ItemsController::class);
+
+    // Supplier routes
+    Route::get('/suppliers', [SuppliersController::class, 'index'])->name('suppliers.index');
+    Route::get('/suppliers/create', [SuppliersController::class, 'create'])->name('suppliers.create');
+    Route::post('/suppliers', [SuppliersController::class, 'store'])->name('suppliers.store');
+    Route::get('/suppliers/{supplier}', [SuppliersController::class, 'show'])->name('suppliers.show');
+    Route::get('/suppliers/{supplier}/edit', [SuppliersController::class, 'edit'])->name('suppliers.edit');
+    Route::put('/suppliers/{supplier}', [SuppliersController::class, 'update'])->name('suppliers.update');
+    Route::delete('/suppliers/{supplier}', [SuppliersController::class, 'destroy'])->name('suppliers.destroy');
 });
 
+// Include auth routes
 require __DIR__.'/auth.php';
-
-
-Route::resource('items', ItemsController::class);
-
-Route::get('/suppliers', [SuppliersController::class, 'index'])->name('suppliers.index');
-Route::get('/suppliers/create', [SuppliersController::class, 'create'])->name('suppliers.create');
-Route::post('/suppliers', [SuppliersController::class, 'store'])->name('suppliers.store');
-Route::get('/suppliers/{supplier}', [SuppliersController::class, 'show'])->name('suppliers.show');
-Route::get('/suppliers/{supplier}/edit', [SuppliersController::class, 'edit'])->name('suppliers.edit');
-Route::put('/suppliers/{supplier}', [SuppliersController::class, 'update'])->name('suppliers.update');
-Route::delete('/suppliers/{supplier}', [SuppliersController::class, 'destroy'])->name('suppliers.destroy');
